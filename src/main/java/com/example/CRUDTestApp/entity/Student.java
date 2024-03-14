d95fd5f155e9fd468e7dda87d44592a1a2c53b5f
kanari <christianaus13@gmail.com>
2024-03-14 19:26:15 +0800

    adding student

---
+++ src/main/java/com/example/CRUDTestApp/entity/Student.java
@@ -0,0 +1,25 @@
+package com.example.CRUDTestApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Books")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentName;
+
+    private String studentCourse;
+
+    private String studentId;
+    
+
+}
