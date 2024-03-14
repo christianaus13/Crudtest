c82285795e6800c451ded943cbcffd9a0de13e6a
kanari <christianaus13@gmail.com>
2024-03-14 19:25:06 +0800

    adding student

--- src/main/java/com/example/CRUDTestApp/controller/studentController.java
+++ src/main/java/com/example/CRUDTestApp/controller/studentController.java
@@ -1,10 +1,55 @@
 package com.example.CRUDTestApp.controller;
 
+import com.example.CRUDTestApp.entity.Student;
+import com.example.CRUDTestApp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class studentController {
+
+    private StudentRepository studentRepository;
+
+    public ResponseEntity<Student> getAllStudent(){
+        try{
+            List<Student> studentList= new ArrayList<>();
+            studentRepository.findAll().forEach(studentList::add);
+
+            if (studentList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return  new ResponseEntity<>(studentList, HttpStatus.OK);
+
+        }catch (Exception ex){
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+    public void getStudentById(){
+
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void updateStudentById(){
+
+    }
+
+    public void deleteStudent(){
+
+    }
+
+
+
 
 }
--- src/main/java/com/example/CRUDTestApp/repository/StudentRepository.java
+++ src/main/java/com/example/CRUDTestApp/repository/StudentRepository.java
@@ -1,10 +1,10 @@
 package com.example.CRUDTestApp.repository;
 
-import com.example.CRUDTestApp.entity.student;
+import com.example.CRUDTestApp.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface studentRepository extends JpaRepository<student,Long> {
+public interface StudentRepository extends JpaRepository<Student,Long> {
 
 }
